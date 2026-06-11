10115f2cf9020ad4feee2c3a1d2bd77e3db7c51c
Piyush Divyankar(MacBook) <piyush0divyankar@gmail.com>
2026-06-11 13:44:19 +0530

    Extending to multiple timers

--- lua/goblin/htimer.lua
+++ lua/goblin/htimer.lua
@@ -1,7 +1,7 @@
 -- lua/goblin/htimer.lua
 local M = {}
 
-local timer = nil
+local timers = {}
 
 local defaults = {
     delay = 2000,
@@ -9,34 +9,51 @@ local defaults = {
     message = "Time to get a drink",
 }
 
-function M.start(opts)
-    if timer then
+function M.start(name, opts)
+    name = name or "default"
+    if timers[name] then
         return -- already running
     end
 
     opts = vim.tbl_deep_extend("force", defaults, opts or {})
 
-    timer = vim.uv.new_timer()
+    local timer = vim.uv.new_timer()
 
     timer:start(
-        opts.delay, -- delay (ms)
+        opts.delay,    -- delay (ms)
         opts.interval, -- repeat (ms)
         vim.schedule_wrap(function()
-            vim.notify("Time to drink some water", vim.log.levels.WARN)
+            vim.notify("Time to drink some water", vim.log.levels.WARN, { title = "Goblin Timer" })
         end)
     )
+    timers[name] = timer
 end
 
-function M.stop()
-    if not timer then
+function M.stop(name)
+    name = name or "default"
+    if not timers[name] then
         return
     end
 
     pcall(function()
-        timer:stop()
-        timer:close()
+        timers[name]:stop()
+        timers[name]:close()
     end)
-    timer = nil
+    timers[name] = nil
+end
+
+function M.stop_all()
+    for name in pairs(timers) do
+        M.stop(name)
+    end
+end
+
+function M.list()
+    local names = {}
+    for name in pairs(timers) do
+        table.insert(names, name)
+    end
+    return names
 end
 
 return M
